841a521e4877ca523ae1a3ba59700897d62e7226
Nikhita <bnikhita2003@gmail.com>
2022-06-25 14:29:32 +0530

    exported pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from "./home/home.pages";
+export { default as Explore } from "./explore/explore.pages";
+export { default as Charity } from "./charity/charity.pages";
+export { default as Bar } from "./bar/bar.pages";
