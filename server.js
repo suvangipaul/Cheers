f767a521da783c96708e86ab1c550704224b162c
suvangipaul <suvangipaul8@gmail.com>
2022-06-25 17:48:41 +0530

    Updated

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cors = require('cors')
 dotenv.config()
+const hotelRoute = require('./routes/hotels.routes')
 
 //configuration
 const PORT = process.env.PORT || 4000
 const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+// database connections
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.once('open', () => {
+    console.log('Database is connected')
+})
+
+//routes
+app.use('/hotels', hotelRoute)
 
 app.get('/', (req, res) => {
     res.send('Server is Running')
