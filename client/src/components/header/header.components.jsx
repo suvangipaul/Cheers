588492f36416f93b06259d4994bc773918a90f85
Nikhita <bnikhita2003@gmail.com>
2022-06-25 14:58:42 +0530

    added header

--- client/src/components/header/header.components.jsx
+++ client/src/components/header/header.components.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import './header.styles.css'
 
 const Header = () => {
-    return (
-    <div>header.components</div>
+  return (
+    <div className="navbar">
+      <div className="brand-logo">
+        Cheers!
+      </div>
+      <div className="nav_options">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/explore">Explore</a>
+          </li>
+          <li>
+            <a href="/bar">Bar</a>
+          </li>
+          <li>
+            <a href="/Charity">Charity</a>
+          </li>
+        </ul>
+      </div>
+        <button className='connect'>Connect Wallet</button>
+    </div>
   )
 }
 
